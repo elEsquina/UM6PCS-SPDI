cfe3ee46839fd2a1ea4898374b9def7d2d82c31d
elEsquina <o.azoubi@hotmail.com>
2024-05-20 11:15:04 +0100

    yes

---
+++ vue-firebase-forum/src/firebase/config.js
@@ -0,0 +1,18 @@
+import  firebase  from "firebase/app";
+import "firebase/firestore";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAy7SyrZCjEbEAoeJcKmY9lHlBIR2aIYkU",
+  authDomain: "spd1testjobs.firebaseapp.com",
+  projectId: "spd1testjobs",
+  storageBucket: "spd1testjobs.appspot.com",
+  messagingSenderId: "73754400890",
+  appId: "1:73754400890:web:184d9d42a7f7131987c3c3",
+  measurementId: "G-PGTNZL5P35"
+};
+
+const fireapp = firebase.initializeApp(firebaseConfig);
+const projectFirestore = fireapp.firestore();
+
+export {firebase, projectFirestore};
